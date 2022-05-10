188b2f0f11ec1e5500a7e060b9243f0056d0eba7
raicleysantana <raicleysantana1@gmail.com>
2022-05-10 15:56:11 -0400

    Inicio do desenvolvimento das escrituras

---
+++ header.php
@@ -0,0 +1,25 @@
+<header id="header" class="header fixed-top" data-scrollto-offset="0">
+    <div class="container-fluid d-flex align-items-center justify-content-between">
+
+        <a href="index.html" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
+            <!-- Uncomment the line below if you also wish to use an image logo -->
+            <!-- <img src="assets/img/logo.png" alt=""> -->
+            <h1>Portal escrituras<span>.</span></h1>
+        </a>
+
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a class="nav-link scrollto" href="/escrituras/">Início</a></li>
+                <li><a class="nav-link scrollto" href="index.html#services">Serviços</a></li>
+                <li><a class="nav-link scrollto" href="index.html#portfolio">Suporte</a></li>
+                <li><a class="nav-link scrollto" href="index.html#about">Seja membro</a></li>
+                <li><a class="nav-link scrollto" href="index.html#contact">Contato</a></li>
+                <li><a class="nav-link scrollto" href="index.html#team">Login</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle d-none"></i>
+        </nav><!-- .navbar -->
+        <a class="btn-getstarted scrollto" href="index.html#about">Cadastre-se</a>
+    </div>
+</header><!-- End Header -->
+
+
