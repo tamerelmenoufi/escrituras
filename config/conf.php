c2aefac9d7990af46d3460c7e8fe9c5620e9df44
raicleysantana <raicleysantana1@gmail.com>
2022-05-16 22:19:15 -0400

    Adicionado jquery no projeto

--- config/conf.php
+++ config/conf.php
@@ -1,5 +1,5 @@
 <?php
 
 $protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https" : "http";
-$host = $_SERVER['HTTP_HOST'] != 'localhost' ? '206.81.10.165/' : 'localhost/escrituras/';
+$host = $_SERVER['HTTP_HOST'] != 'localhost' ? '206.81.10.165/' : 'localhost/escrituras';
 $base_url = $protocol . "://" . $host . '/';
