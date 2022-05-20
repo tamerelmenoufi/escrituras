07bdfe984ae956b6fc12785e3eab6af3360f8419
raicleysantana <raicleysantana1@gmail.com>
2022-05-19 21:15:03 -0400

    Ajustes na conexao

--- config/ConnectionMySQL.php
+++ config/ConnectionMySQL.php
@@ -13,7 +13,7 @@ const PASSWORD = "";
 $con = mysqli_connect(HOST, USER, PASSWORD, DBNAME) or die(mysqli_error($con));
 
 // Check connection
-/*if ($con->connect_errno) {
+if ($con->connect_errno) {
     echo "Failed to connect to MySQL: " . $con->connect_error;
     exit();
-}*/
+}
