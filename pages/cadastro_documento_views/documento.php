26729a3bf48778bd9e57047a502b8779d4b737df
raicleysantana <raicleysantana1@gmail.com>
2022-05-19 21:21:44 -0400

    Ajustes

--- pages/cadastro_documento_views/documento.php
+++ pages/cadastro_documento_views/documento.php
@@ -1,28 +1,27 @@
-
 <div class="mb-3">
     <label for="cartorio" class="form-label">Cartório</label>
     <input
-        type="text"
-        class="form-control"
-        id="cartorio"
-        name="cartorio"
-        aria-describedby="cartorio"
-        required
+            type="text"
+            class="form-control"
+            id="cartorio"
+            name="cartorio"
+            aria-describedby="cartorio"
+            required
     >
 </div>
 
 <div class="mb-3">
     <label for="tipo_documento" class="form-label">Tipo de documento</label>
     <select
-        class="form-control"
-        id="tipo_documento"
-        name="tipo_documento"
-        aria-describedby="tipo_documento"
-        required
+            class="form-control"
+            id="tipo_documento"
+            name="tipo_documento"
+            aria-describedby="tipo_documento"
+            required
     >
         <option value=""></option>
         <?php
-        $query_tipo_documento = "SELECT * FROM aux_tipo_documento WHERE deletado = '1'";
+        $query_tipo_documento = "SELECT * FROM aux_tipo_documento WHERE deletado != '1'";
         $result = mysqli_query($con, $query_tipo_documento);
 
         while ($row = mysqli_fetch_object($result)): ?>
@@ -35,19 +34,19 @@
 <div class="mb-3">
     <label for="tipo_imovel" class="form-label">Tipo de Imóvel</label>
     <select
-        type="text"
-        class="form-control"
-        id="tipo_imovel"
-        name="tipo_imovel"
-        aria-describedby="tipo_imovel"
+            type="text"
+            class="form-control"
+            id="tipo_imovel"
+            name="tipo_imovel"
+            aria-describedby="tipo_imovel"
     >
         <option value=""></option>
         <?php
-        $query_tipo_imovel = "SELECT * FROM aux_tipo_imovel WHERE situacao != '1'";
-        $result = mysqli_query($con, $query_tipo_documento);
+        $query_tipo_imovel = "SELECT * FROM aux_tipo_imovel WHERE deletado != '1'";
+        $result = mysqli_query($con, $query_tipo_imovel);
 
         while ($row = mysqli_fetch_object($result)): ?>
-            <option value="<?= $row->codigo ?>"><?= $row->nome ?></option>
+            <option value="<?= $row->codigo ?>"><?= $row->descricao ?></option>
         <?php endwhile; ?>
     </select>
 </div>
@@ -55,11 +54,11 @@
 <div class="mb-3">
     <label for="nivel_imovel" class="form-label">Nivel do imóvel</label>
     <input
-        type="number"
-        min="0"
-        class="form-control"
-        id="nivel_imovel"
-        name="nivel_imovel"
-        aria-describedby="nivel_imovel"
+            type="number"
+            min="0"
+            class="form-control"
+            id="nivel_imovel"
+            name="nivel_imovel"
+            aria-describedby="nivel_imovel"
     >
 </div>
