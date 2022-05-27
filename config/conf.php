66a4169bd03e41f6f68ecb5d13e791457efdb464
raicleysantana <raicleysantana1@gmail.com>
2022-05-27 00:37:47 -0400

    Atualizações

--- config/conf.php
+++ config/conf.php
@@ -3,3 +3,10 @@
 $protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https" : "http";
 $host = $_SERVER['HTTP_HOST'] != 'localhost' ? '206.81.10.165/' : 'localhost/escrituras';
 $base_url = $protocol . "://" . $host . '/';
+
+
+if (session_id() === "") {
+    session_start();
+}
+
+date_default_timezone_set('America/Manaus');
---
+++ pages/actions/actionCadastro_documento.php
@@ -0,0 +1,30 @@
+<?php
+include '../../config/includes.php';
+
+$data = $_POST;
+$attr = [];
+
+foreach ($data as $name => $value) {
+    $attr[] = "{$name} = '" . mysqli_real_escape_string($con, $value) . "'";
+}
+
+$attr = implode(', ', $attr);
+
+$sql = "INSERT INTO documentos SET {$attr}";
+
+if (mysqli_query($con, $sql)) {
+    $_SESSION['alert'] = [
+        "title" => "Sucesso",
+        "content" => "Documento salvo com sucesso"
+    ];
+
+    echo "ok";
+} else {
+    $_SESSION['alert'] = [
+        "title" => "Error",
+        "content" => "Error ao salvar documento"
+    ];
+    echo "error";
+}
+
+exit();
