94f234c7f68ea65c13f5076fe6ae10f831f7b8fd
raicleysantana <raicleysantana@solasstec.com.br>
2022-05-30 09:04:35 -0400

    Atualização

--- config/ConnectionMySQL.php
+++ config/ConnectionMySQL.php
@@ -1,19 +1,21 @@
 <?php
 
-// ENDERECO DO SERVIDOR
-const HOST = "localhost";
-// NOME DO SCHEMA DB
-const DBNAME = "portal_escritura";
-// USUARIO
-const USER = "root";
-// SENHA
-const PASSWORD = "";
-
+if ($_SERVER['HTTP_HOST'] === 'localhost') {
+    define('DB_HOST', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_DATABASE', 'portal_escritura');
+} else {
+    define('DB_HOST', '206.81.10.165');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '53nh@D0B@nc0');
+    define('DB_DATABASE', 'portal_escritura');
+}
 
 $con = mysqli_connect(HOST, USER, PASSWORD, DBNAME) or die(mysqli_error($con));
 
 // Check connection
-if ($con->connect_errno) {
-    echo "Failed to connect to MySQL: " . $con->connect_error;
+if (mysqli_connect_error($con)) {
+    echo "Failed to connect to MySQL";
     exit();
 }
