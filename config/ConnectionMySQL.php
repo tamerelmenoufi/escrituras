4a01cb725ae01645d8781c7376051580235e1894
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-30 21:40:16 -0400

    Atualizações

--- config/ConnectionMySQL.php
+++ config/ConnectionMySQL.php
@@ -2,23 +2,23 @@
 
 error_reporting(E_ALL);
 
-// if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '10.0.0.115') {
-//     define('HOST', 'localhost');
-//     define('USER', 'root');
-//     define('PASSWORD', '');
-//     define('DBNAME', 'portal_escritura');
-// } else {
-//     define('HOST', 'escrituras.mohatron.com');
-//     define('USER', 'root');
-//     define('PASSWORD', '53nh@D0B@nc0');
-//     define('DBNAME', 'portal_escritura');
-// }
+if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '10.0.0.115') {
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASSWORD', '');
+    define('DBNAME', 'portal_escritura');
+} else {
+    define('HOST', 'escrituras.mohatron.com');
+    define('USER', 'root');
+    define('PASSWORD', '53nh@D0B@nc0');
+    define('DBNAME', 'portal_escritura');
+}
 
 
-// $con = mysqli_connect(HOST, USER, PASSWORD, DBNAME) or die(mysqli_error($con));
+$con = mysqli_connect(HOST, USER, PASSWORD, DBNAME) or die(mysqli_error($con));
 
-// // Check connection
-// if (mysqli_connect_error($con)) {
-//     echo "Failed to connect to MySQL";
-//     exit();
-// }
+// Check connection
+if (mysqli_connect_error($con)) {
+    echo "Failed to connect to MySQL";
+    exit();
+}
