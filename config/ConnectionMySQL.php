88dd30038fb94a1a35b0c180c1214a98cc7570ca
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-30 21:51:47 -0400

    Atualizações

--- config/ConnectionMySQL.php
+++ config/ConnectionMySQL.php
@@ -1,29 +1,23 @@
 <?php
 
-// error_reporting(E_ALL);
+error_reporting(E_ALL);
 
-// if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '10.0.0.115') {
-//     define('HOST', 'localhost');
-//     define('USER', 'root');
-//     define('PASSWORD', '');
-//     define('DBNAME', 'portal_escritura');
-// } else {
-//     define('HOST', 'escrituras.mohatron.com');
-//     define('USER', 'root');
-//     define('PASSWORD', '53nh@D0B@nc0');
-//     define('DBNAME', 'portal_escritura');
-// }
+if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '10.0.0.115') {
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASSWORD', '');
+    define('DBNAME', 'portal_escritura');
+} else {
+    define('HOST', 'escrituras.mohatron.com');
+    define('USER', 'root');
+    define('PASSWORD', '53nh@D0B@nc0');
+    define('DBNAME', 'portal_escritura');
+}
 
-$HOST = 'escrituras.mohatron.com';
-$USER = 'root';
-$PASSWORD = '53nh@D0B@nc0';
-$DBNAME = 'portal_escritura';
-
-
-$con = mysqli_connect($HOST, $USER, $PASSWORD, $DBNAME) or die(mysqli_error($con));
+$con = mysqli_connect(HOST, USER, PASSWORD, DBNAME); // or die(mysqli_error($con));
 
 // Check connection
-if (mysqli_connect_error($con)) {
-    echo "Failed to connect to MySQL";
-    exit();
-}
+// if (mysqli_connect_error($con)) {
+//     echo "Failed to connect to MySQL";
+//     exit();
+// }
