9213590dcb7d584de039df79d277f65248390c2d
raicleysantana <raicleysantana@solasstec.com.br>
2022-05-31 08:45:05 -0400

    Ajustes

--- pages/lista/bairros.php
+++ pages/lista/bairros.php
@@ -6,7 +6,7 @@ $cidade = $_POST['valor'];
 $html = '<option value=""></option>';
 
 if ($cidade) {
-    $result = mysqli_query($con, "SELECT * FROM aux_bairros WHERE situacao = '1' AND cidades = '{$cidade}'");
+    $result = mysqli_query($con, "SELECT * FROM aux_bairros WHERE situacao = '1' AND cidade = '{$cidade}'");
 
     while ($row = mysqli_fetch_object($result)) {
         $html .= '<option value="' . $row->codigo . '">' . $row->nome . '</option>';
--- pages/lista/cidades.php
+++ pages/lista/cidades.php
@@ -6,7 +6,7 @@ $estado = $_POST['valor'];
 $html = '<option value=""></option>';
 
 if ($estado) {
-    $result = mysqli_query($con, "SELECT * FROM aux_cidades WHERE situacao = '0' AND estados = '{$estado}'");
+    $result = mysqli_query($con, "SELECT * FROM aux_cidades WHERE situacao = '1' AND estado = '{$estado}'");
 
     while ($row = mysqli_fetch_object($result)) {
         $html .= '<option value="' . $row->codigo . '">' . $row->nome . '</option>';
