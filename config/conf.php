b809a1ee86e6bfed0190385b67471dab9f45061d
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-30 21:38:45 -0400

    Atualizações

--- config/conf.php
+++ config/conf.php
@@ -1,7 +1,7 @@
 <?php
 
 $protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https" : "http";
-$host = $_SERVER['HTTP_HOST'] != 'localhost' ? '206.81.10.165/' : 'localhost/escrituras';
+$host = $_SERVER['HTTP_HOST'] != 'localhost' ? 'escrituras.mohatron.com/' : 'localhost/escrituras';
 $base_url = $protocol . "://" . $host . '/';
 
 
