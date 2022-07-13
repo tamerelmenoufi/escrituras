58e8be24b29a37c45292c8531586c755e5e6c1eb
tamerelmenoufi <69819013+tamerelmenoufi@users.noreply.github.com>
2022-07-13 15:32:04 -0400

    Update main.php

--- main.php
+++ main.php
@@ -75,9 +75,7 @@
         <div class="section-header">
             <h2>Sobre nós</h2>
             <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book.
+                Conteúdo sobre nós ......
             </p>
         </div>
 
