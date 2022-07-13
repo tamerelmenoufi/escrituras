99236af6da60d32c90d4c775851f94b189d7af74
tamerelmenoufi <69819013+tamerelmenoufi@users.noreply.github.com>
2022-07-13 15:36:52 -0400

    Update main.php

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <section id="hero-fullscreen" class="hero-fullscreen d-flex align-items-center">
     <div class="container d-flex flex-column align-items-center position-relative" data-aos="zoom-out">
-        <h2 class="text-center">PORTAL ESCRITURA ATUALIZA</h2>
+        <h2 class="text-center">PORTAL ESCRITURA</h2>
         <p>
             Sua forma mais rápida e eficiente de localizar a Escritura Pública de um imóvel
             
