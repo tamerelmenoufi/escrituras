60bc00ef050f9d0402f0ce8c86f8b99ad33938ae
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-16 08:25:38 -0400

    Atualizações

--- pages/documentos/_form_comprador.php
+++ pages/documentos/_form_comprador.php
@@ -56,7 +56,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' and $_POST['acao'] === 'excluir'){
 
 
 if($_GET['acao'] == 'novo'){
-    echo $q = "INSERT INTO vendedor_comprador set documento_id = '{$_SESSION['id']}', tipo = 'c'";
+    $q = "INSERT INTO vendedor_comprador set documento_id = '{$_SESSION['id']}', tipo = 'c'";
     mysqli_query($con, $q);
     $_GET['comprador_id'] = mysqli_insert_id();
     echo $_GET['comprador_id'];
--- pages/documentos/_form_vendedor.php
+++ pages/documentos/_form_vendedor.php
@@ -53,7 +53,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' and $_POST['acao'] === 'excluir'){
 }
 
 if($_GET['acao'] == 'novo'){
-    echo $q = "INSERT INTO vendedor_comprador set documento_id = '{$_SESSION['id']}', tipo = 'v'";
+    $q = "INSERT INTO vendedor_comprador set documento_id = '{$_SESSION['id']}', tipo = 'v'";
     mysqli_query($con, $q);
     $_GET['vendedor_id'] = mysqli_insert_id();
     echo $_GET['vendedor_id'];
