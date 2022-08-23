656fb762cee1164e10ee25641c7b918926b0650a
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-23 08:35:14 -0400

    Atualizações

--- pages/busca-escritura.php
+++ pages/busca-escritura.php
@@ -5,6 +5,22 @@
         Seja bem vindo, preencha alguns parâmetros para fazermos sua busca!
     </h5>
 
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">Dropdown</button>
+            <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+            <div role="separator" class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Separated link</a>
+            </div>
+        </div>
+        <input type="text" class="form-control" aria-label="Text input with dropdown button">
+    </div>
+
+
+
     <form method="post" action="actions/teste.php">
         <div class="row">
             <div class="col-md-6 offset-md-3">
