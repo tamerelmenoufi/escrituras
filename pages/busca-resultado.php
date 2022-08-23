55f983c0348a8706b031716d82efe2f97a1e7f13
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-23 10:05:22 -0400

    Atualizações

--- pages/busca-resultado.php
+++ pages/busca-resultado.php
@@ -3,5 +3,35 @@ include_once "./config/conf.php";
 ?>
 <div class="container py-4">
     <b class="text-left mb-3">Resultado da busca por <?=$_POST['busca']?> no campo <?=$_POST['campo']?></b>
-    <div class="info p-4">Aqui o conteúdo ...</div>
+    <div class="info p-4">
+
+        <div class="list-group">
+            <a href="#" class="list-group-item list-group-item-action">
+                <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small>3 days ago</small>
+                </div>
+                <p class="mb-1">Some placeholder content in a paragraph.</p>
+                <small>And some small print.</small>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action">
+                <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small class="text-muted">3 days ago</small>
+                </div>
+                <p class="mb-1">Some placeholder content in a paragraph.</p>
+                <small class="text-muted">And some muted small print.</small>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action">
+                <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small class="text-muted">3 days ago</small>
+                </div>
+                <p class="mb-1">Some placeholder content in a paragraph.</p>
+                <small class="text-muted">And some muted small print.</small>
+            </a>
+        </div>
+
+
+    </div>
 </div>
