1f1cf64207875431433feeb8bc1b88fbc378b362
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-23 08:20:19 -0400

    Atualizações

--- header.php
+++ header.php
@@ -24,10 +24,10 @@
                         <?php } ?>
                     </ul>
                 </li>
-                <li><a class="nav-link scrollto" href="index.html#portfolio">Suporte</a></li>
+                <li><a class="nav-link scrollto" href="index.php#portfolio">Suporte</a></li>
                 <li><a class="nav-link scrollto" href="seja-membro">Seja membro</a></li>
                 <li><a class="nav-link scrollto" href="quem-somos">Quem somos</a></li>
-                <li><a class="nav-link scrollto" href="contato">Contato</a></li>
+                <li><a class="nav-link scrollto" href="index.php#contato">Contato</a></li>
                 <?php if (!isset($_SESSION['usuario'])) { ?>
                     <li><a class="nav-link scrollto" href="login">Login</a></li>
                 <?php } ?>
