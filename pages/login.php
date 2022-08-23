283a77c80a8aaa4d5d11f69c6708aa256fe1ac8b
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-22 23:09:24 -0400

    Atualizações

--- pages/login.php
+++ pages/login.php
@@ -12,7 +12,8 @@ include_once "./config/conf.php";
 <div class="container my-5">
     <div class="row">
         <div class="col-md-6">
-            <div style="border-radius:8px; margin-left:50px; width:100%; height:150px; background-color:#000000; margin-top:15%; padding:20px;">
+            <div style="border-radius:8px; margin-left:50px; width:100%; height:150px; background-color:#000000a8; margin-top:15%; padding:20px;">
+                <p>Ainda não tem cadastro?</p>
                 <button class="btn btn-success" type="button" style="width:calc(100% - 50px); margin-top:30px;">FAÇA SEU CADASTRO</button>
             </div>
         </div>
