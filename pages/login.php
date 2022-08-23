d6fd7735ac77a75838aebaf9bc39f4427ab3505e
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-22 21:43:52 -0400

    Atualizações

--- pages/login.php
+++ pages/login.php
@@ -11,62 +11,66 @@ include_once "./config/conf.php";
 </style>
 <div class="container my-5">
     <div class="row">
+        <div class="col-md-6">
 
-        <div class="card">
-            <div class="offset-lg-3 col-lg-6" style="margin-top: 1rem;margin-bottom: 3rem">
-                <form id="form-login" method="post" role="form" class="">
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="offset-lg-3 col-lg-6" style="margin-top: 1rem;margin-bottom: 3rem">
+                    <form id="form-login" method="post" role="form" class="">
 
-                    <input type="hidden" name="acao" value="login">
+                        <input type="hidden" name="acao" value="login">
 
-                    <div class="row">
-                        <h3
-                                class="text-center"
-                                style="color: rgba(var(--color-secondary-dark-rgb), 1);"
-                        >
-                            Área de login
-                        </h3>
+                        <div class="row">
+                            <h3
+                                    class="text-center"
+                                    style="color: rgba(var(--color-secondary-dark-rgb), 1);"
+                            >
+                                Área de login
+                            </h3>
 
-                        <div class="form-group mt-3">
-                            <div class="alert alert-danger alert-dismissible msg-error" role="alert"
-                                style="display: none">
-                                <text></text>
-                                <button type="button" class="btn-close" aria-label="Close"></button>
+                            <div class="form-group mt-3">
+                                <div class="alert alert-danger alert-dismissible msg-error" role="alert"
+                                    style="display: none">
+                                    <text></text>
+                                    <button type="button" class="btn-close" aria-label="Close"></button>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <input
+                                        type="text"
+                                        name="nome"
+                                        class="form-control"
+                                        id="nome"
+                                        maxlength="45"
+                                        placeholder="Usuário"
+                                        onfocus=""
+                                        style="color: rgba(var(--color-secondary-dark-rgb), 0.7)"
+                                        required>
+                            </div>
+
+                            <div class="form-group mt-3">
+                                <input
+                                        type="password"
+                                        class="form-control"
+                                        name="senha"
+                                        id="senha"
+                                        maxlength="150"
+                                        placeholder="Senha"
+                                        style="color: rgba(var(--color-secondary-dark-rgb), 0.7)"
+                                        required
+                                >
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <input
-                                    type="text"
-                                    name="nome"
-                                    class="form-control"
-                                    id="nome"
-                                    maxlength="45"
-                                    placeholder="Usuário"
-                                    onfocus=""
-                                    style="color: rgba(var(--color-secondary-dark-rgb), 0.7)"
-                                    required>
+                        <div class="text-center mt-4 mt-3">
+                            <button class="button-primary" type="submit" style="width: 50%">Entrar</button>
                         </div>
 
-                        <div class="form-group mt-3">
-                            <input
-                                    type="password"
-                                    class="form-control"
-                                    name="senha"
-                                    id="senha"
-                                    maxlength="150"
-                                    placeholder="Senha"
-                                    style="color: rgba(var(--color-secondary-dark-rgb), 0.7)"
-                                    required
-                            >
-                        </div>
-                    </div>
-
-                    <div class="text-center mt-4 mt-3">
-                        <button class="button-primary" type="submit" style="width: 50%">Entrar</button>
-                    </div>
-
-                </form>
-            </div><!-- End Contact Form -->
+                    </form>
+                </div><!-- End Contact Form -->
+            </div>
         </div>
     </div>
 </div>
