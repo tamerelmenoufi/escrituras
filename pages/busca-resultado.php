f864f4de0ad5cf1b522371ce9e6d9feca3f64799
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-23 09:49:34 -0400

    Atualizações

--- pages/busca-resultado.php
+++ pages/busca-resultado.php
@@ -2,8 +2,6 @@
 include_once "./config/conf.php";
 ?>
 <div class="container py-4">
-    <h2 class="text-center mb-3">Resultado da busca por <?=$_POST['busca']?></h2>
-
+    <b class="text-left mb-3">Resultado da busca por <?=$_POST['busca']?></b>
     <div class="info p-4">Aqui o conteúdo ...</div>
-
 </div>
