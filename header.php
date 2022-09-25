e3c39aacfc35c6984a2f532a942ca633ade5c9e0
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-24 20:23:00 -0400

    Atualizações

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
         <a href="./" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
             <!-- Uncomment the line below if you also wish to use an image logo -->
             <!-- <img src="assets/img/logo.png" alt=""> -->
-            <h1>Portal escrituras<span>.</span></h1>
+            <h1>Portal Escrituras<span>.</span></h1>
         </a>
 
         <nav id="navbar" class="navbar">
